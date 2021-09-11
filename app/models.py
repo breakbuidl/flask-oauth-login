11ddbba65ec147d95f01a5b30aacb5d2890cd594
Pinank Solanki <solankipinank10@gmail.com>
2021-09-11 16:54:22 +0530

    Implement user signup/login functionality.

--- app/models.py
+++ app/models.py
@@ -1,7 +1,9 @@
 from app import db
+from app import login
+from flask_login import UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
 
-class User(db.Model):
+class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
@@ -15,3 +17,7 @@ class User(db.Model):
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
+
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
