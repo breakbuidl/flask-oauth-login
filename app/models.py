fc4cda32eaf9a7a40c45e49449498a5f292646d8
Pinank Solanki <solankipinank10@gmail.com>
2021-09-14 01:40:02 +0530

    Change format for User's __repr__()

--- app/models.py
+++ app/models.py
@@ -17,7 +17,7 @@ class User(UserMixin, db.Model):
         return check_password_hash(self.password_hash, password)
 
     def __repr__(self):
-        return '<User {}>'.format(self.username)
+        return '<User {} {} {}>'.format(self.username, self.email, self.name)
 
 @login.user_loader
 def load_user(id):
